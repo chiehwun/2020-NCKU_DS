5ed30a4d4bd69e1bc95410fa22d17faa1579473c
chiehwun <chiehwun5200@gmail.com>
2020-12-02 20:34:44 +0800

    Update HW6 2020/12/2 20:34

--- HW6_AVL_tree/code/hw6.c
+++ HW6_AVL_tree/code/hw6.c
@@ -22,10 +22,11 @@ int main()
     char name[21] = "";
     char phone[11] = "";
     char instr[2] = "";
+    int is_first_print = 1;
     // Construct AVL tree
     while (1)
     {
-        // fetch instruction
+        /* 1. Fetch Instruction */
         fscanf(infile_src, "%s", name);
         // E: program terminate
         if (strcmp("E", name) == 0)
@@ -34,18 +35,21 @@ int main()
             break;
         }
         // judge instruction
-        if (strcmp("D", name) == 0 || strcmp("S", name) == 0)
+        else if (strcmp("D", name) == 0 || strcmp("S", name) == 0)
         {
             if (instr[0] == 'D')
             {
-                // visualize_tree(stderr, root, 9);
+                if (!is_first_print)
+                    fprintf(outfile_src, "\n");
                 print_tree_PRE(outfile_src, root, 0); // end of D
+                is_first_print = 0;                   // toggle flag
             }
             // update instruction
             strcpy(instr, name);
-            // D & S: Read name at first
-            fscanf(infile_src, "%s", name);
+
+            continue;
         }
+        /* Instruction Implementation */
         // D: construct AVL tree
         if (instr[0] == 'D')
         {
@@ -56,15 +60,16 @@ int main()
             visualize_tree(stderr, root, 9);
 #endif
         }
-        // S: search AVL tree
+        // S: search element and print
         else if (instr[0] == 'S')
         {
             // Search in AVL tree
             Node_tr *res = search(root, name);
-            if (res) // Node exists
-                fprintf(outfile_src, "\n%s %s", res->name, res->phone);
-            else
-                fprintf(outfile_src, "\n%s null", name);
+            fprintf(outfile_src,
+                    "%s%s %s", is_first_print ? "" : "\n",
+                    name,
+                    res ? res->phone : "null");
+            is_first_print = 0; // toggle flag
         }
     }
     return 0;
